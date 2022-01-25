8ec8d5031378e80a265a726ba0f22d0992ce1575
Jeff Cho <58960199+HardlyEasy@users.noreply.github.com>
2022-01-25 13:50:01 -0500

    Python O(n^2) done

--- src/leetcode/medium/longestpalindromicsubstring5/Solution.py
+++ src/leetcode/medium/longestpalindromicsubstring5/Solution.py
@@ -1,3 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        return ''
+        if len(s) == 1:
+            return s
+        longest = ''
+        longest_len = 0
+        for i in range(0, len(s), +1):
+            # odd length
+            l, r = i, i
+            # while in bounds and is palindrome
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                curr = s[l:r+1]
+                if len(curr) > longest_len:
+                    longest_len = len(curr)
+                    longest = curr
+                l -= 1
+                r += 1
+        for i in range(0, len(s), +1):
+            # even length
+            l, r = i, i + 1
+            # while in bounds and is palindrome
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                curr = s[l:r+1]
+                if len(curr) > longest_len:
+                    longest_len = len(curr)
+                    longest = curr
+                l -= 1
+                r += 1
+        return longest
--- src/leetcode/medium/longestpalindromicsubstring5/Test.py
+++ src/leetcode/medium/longestpalindromicsubstring5/Test.py
@@ -1,3 +1,5 @@
 from leetcode.medium.longestpalindromicsubstring5.Solution import Solution
 
 s = Solution()
+print(s.longestPalindrome("babad"))
+print(s.longestPalindrome("cbbd"))
