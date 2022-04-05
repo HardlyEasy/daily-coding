d036d11e894c2aa1dc71c0bdde3984b118622767
jeff <jeffcho36@gmail.com>
2022-04-05 16:04:20 -0400

    Done: lc_e_136 cs

---
+++ src/leetcode/easy/singlenumber136/cs/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        Hashtable ht = new Hashtable();
+
+        foreach (int i in nums) {
+            if (ht.ContainsKey(i))
+                ht.Remove(i);
+            else
+                ht.Add(i, 1);
+        }
+        foreach(DictionaryEntry e in ht)
+            return (int)e.Key;
+        return -99;
+    }
+}
---
+++ src/leetcode/easy/singlenumber136/cs/Test.cs
@@ -0,0 +1,11 @@
+﻿public class Test {
+    public static void Main(String[] args) {
+        Solution s = new Solution();
+        int[] ex1 = {2, 2, 1};
+        int[] ex2 = {4, 1, 2, 1, 2};
+        int[] ex3 = {1};
+        System.Console.WriteLine(s.SingleNumber(ex1));
+        System.Console.WriteLine(s.SingleNumber(ex2));
+        System.Console.WriteLine(s.SingleNumber(ex3));
+    }
+}
