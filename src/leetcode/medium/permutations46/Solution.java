31bb2b2ee27881b568764cabe47fa6ba38b23b19
jeff <jeffcho36@gmail.com>
2022-04-06 17:36:46 -0400

    Partial: lc_m_46 java

---
+++ src/leetcode/medium/permutations46/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.medium.permutations46;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+
+        return answer;
+    }
+}
---
+++ src/leetcode/medium/permutations46/Test.java
@@ -0,0 +1,11 @@
+package leetcode.medium.permutations46;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] ex1 = {1, 2, 3};
+        int[] ex2 = {0, 1};
+        int[] ex3 = {1};
+        System.out.println(s.permute(ex1));
+    }
+}
